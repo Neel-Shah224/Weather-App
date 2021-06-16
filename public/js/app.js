f8d2f356f568be464828f46087dce26874ed3d6e
Neel-Shah224 <Neel-Shah224@users.noreply.github.com>
2021-06-16 16:41:07 +0530

    changed error style

--- public/js/app.js
+++ public/js/app.js
@@ -8,9 +8,7 @@ const msg2 = document.querySelector('#msg2')
 weatherForm.addEventListener('submit',(e) => {
     e.preventDefault()
     const location = search.value
-    msg1.style.backgroundColor = 'white'
     msg1.style.color = msg2.style.color
-    msg1.style.padding = msg2.style.padding
     msg2.textContent=''
     msg1.textContent='Loading .... '
     fetch('/weather?address='+location).then( (response) => {
@@ -18,9 +16,7 @@ weatherForm.addEventListener('submit',(e) => {
             if(data.error){
                 msg1.textContent=data.error
                 msg2.textContent=''
-                msg1.style.backgroundColor = 'red'
-                msg1.style.color = 'white'
-                msg1.style.padding = '3px'
+                msg1.style.color = 'red'
             }
             else {
                 msg1.textContent =(data.location )
